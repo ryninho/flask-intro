72236c9e3481775550a2a43bcc673804731a0452
ryninho <eric.rynerson@gmail.com>
2015-09-05 18:24:01 -0700

    logout

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 
 app = Flask(__name__)
 
@@ -20,8 +20,15 @@ def login():
     if request.form['username'] != 'admin' or request.form['password'] != 'admin':
       error = 'Invalid credentials.  Please try again.'
     else:
+      session['logged_in'] = True
       return redirect(url_for('home'))
   return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+  session.pop('logged_in', None)
+  return redirect(url_for('welcome'))
 
 
 if __name__ == '__main__':
