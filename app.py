af7693349900d0c74fcf71e422730e3a2b1db488
ryninho <eric.rynerson@gmail.com>
2015-09-06 13:42:59 -0700

    key is temporary solution, nothing confidential

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
 
 app = Flask(__name__)
 
+app.secret_key = "quickstart-key"
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
 
 @app.route('/')
+@login_required
 def home():
-  return "Hello, world!"
+  # return "Hello, world!"
+  return render_template('index.html')
 
 
 @app.route('/welcome')
@@ -21,13 +36,16 @@ def login():
       error = 'Invalid credentials.  Please try again.'
     else:
       session['logged_in'] = True
+      flash('You were just logged in!')
       return redirect(url_for('home'))
   return render_template('login.html', error=error)
 
 
 @app.route('/logout')
+@login_required
 def logout():
   session.pop('logged_in', None)
+  flash('You were just logged out!')
   return redirect(url_for('welcome'))
 
 
