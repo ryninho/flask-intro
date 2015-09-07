e3203847686ada155711e3518760627066fc8152
ryninho <eric.rynerson@gmail.com>
2015-09-06 23:11:45 -0700

    test suite

---
+++ test.py
@@ -0,0 +1,79 @@
+from app import app
+import unittest
+
+
+class FlaskTestCase(unittest.TestCase):
+
+    # Ensure that flask was set up correctly
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/login', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    # Ensure that the login page loads correctly
+    def test_login_page_loads(self):
+        tester = app.test_client(self)
+        response = tester.get('/login', content_type='html/text')
+        self.assertTrue('Please login' in response.data)
+
+    # Ensure login behaves correctly given the correct credentials
+    def test_correct_login(self):
+        tester = app.test_client(self)
+        response = tester.post(
+          '/login',
+          data=dict(username="admin", password="admin"),
+          follow_redirects=True
+          )
+        self.assertTrue('You were just logged in' in response.data)
+
+    # Ensure login behaves correctly given the INcorrect credentials
+    def test_incorrect_login(self):
+        tester = app.test_client(self)
+        response = tester.post(
+          '/login',
+          data=dict(username="wrong", password="wrong"),
+          follow_redirects=True
+          )
+        self.assertTrue('Please try again' in response.data)
+
+    # Ensure logout behaves correctly
+    def test_correct_logout(self):
+        tester = app.test_client(self)
+        tester.post(
+          '/login',
+          data=dict(username="admin", password="admin"),
+          follow_redirects=True
+          )
+        response = tester.get('/logout', follow_redirects=True)
+        self.assertTrue('You were just logged out' in response.data)
+
+    # Ensure that the main page requires login
+    def test_main_route_requires_login(self):
+        tester = app.test_client(self)
+        response = tester.get('/', follow_redirects=True)
+        self.assertTrue('You need to login first' in response.data)
+
+    # Ensure that the logout page requires user to be logged in
+    def test_logout_page_requires_login(self):
+        tester = app.test_client(self)
+        response = tester.get('/logout', follow_redirects=True)
+        self.assertTrue('You need to login first' in response.data)
+
+    # alternatively could use "assertIn"
+    # def test_logout_route_requires_login(self):
+    #     tester = app.test_client()
+    #     response = tester.get('/logout', follow_redirects=True)
+    #     self.assertIn(b'You need to login first.', response.data)
+
+    # Ensure that posts show up on the main page
+    def test_post_show_up(self):
+        tester = app.test_client(self)
+        response = tester.post(
+          '/login',
+          data=dict(username="admin", password="admin"),
+          follow_redirects=True
+          )
+        self.assertIn('Hello from the shell', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
