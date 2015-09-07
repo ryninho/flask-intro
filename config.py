78db8cd3704a183b3bac79124d982381355fa639
ryninho <eric.rynerson@gmail.com>
2015-09-07 14:47:18 -0700

    follow demo with key for now

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 #default config
 class BaseConfig(object):
   DEBUG = False
-  SECRET_KEY = os.environ['FLASK_INTRO_SECRET_KEY']
+  SECRET_KEY = '\x98\x9f\x83\xff\xdd?\xaf\xb7\x9c\xfd)\x8b\xb0X\xe9y\r\x14A%\x17\x1a\xa5\xbd' # os.environ['FLASK_INTRO_SECRET_KEY']
   SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevelopmentConfig(BaseConfig):
