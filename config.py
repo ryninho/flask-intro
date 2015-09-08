3997cfa603e2358f37eed3fde09d09571d5cf535
ryninho <eric.rynerson@gmail.com>
2015-09-07 23:50:40 -0700

    use url for db

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ import os
 class BaseConfig(object):
   DEBUG = False
   SECRET_KEY = '\x98\x9f\x83\xff\xdd?\xaf\xb7\x9c\xfd)\x8b\xb0X\xe9y\r\x14A%\x17\x1a\xa5\xbd' # os.environ['FLASK_INTRO_SECRET_KEY']
-  SQLALCHEMY_DATABASE_URI = 'sqlite:///posts.db'
-  # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+  # SQLALCHEMY_DATABASE_URI = 'sqlite:///posts.db'
+  SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevelopmentConfig(BaseConfig):
   DEBUG = True
